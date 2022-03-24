abf11dd501ff3819b01475926176159ca63360ee
Abeetha013 <abeethaasahara@gmail.com>
2022-03-24 15:38:59 +0530

    first commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getAllProducts, 
+    getproductById,
+} = require('../controller/productControllers');
+
+
+router.get("/", getAllProducts)
+
+router.get("/:id", getproductById)
+
+module.exports = router;
