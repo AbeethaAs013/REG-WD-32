274f07063973c04febffb2e4b75315c8d27ede47
ashenisandeepa <sandeepaasheni@gmail.com>
2022-04-13 23:26:13 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CounterClass from './components/CounterClass';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello React</h1>
+     <CounterClass/>
     </div>
   );
 }
