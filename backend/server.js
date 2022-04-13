0cecda1c527fa19d6e431773aec1bdc9bb6f2a9f
ashenisandeepa <sandeepaasheni@gmail.com>
2022-04-13 19:09:19 +0530

    commit 1

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,41 @@
-require("dotenv").config();
-const express = require('express');
-const connectDB = require('./config/db');
-const productRoutes = require("./routes/productRoutes");
-
-connectDB();
+const express = require("express");  // variable declaration 
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");  
+const cors = require( "cors");
+const dotenv = require("dotenv");
 
 const app = express();
+require("dotenv").config();
 
-app.use(express.json());
+//server connection
 
-app.use("/api/products", productRoutes);
+const PORT = process.env.PORT || 8070; // port when running in the computer , ||  logical OR
 
-const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL; // DB connection
+
+mongoose.connect(URL, {
+//useCreateIndex : true,
+//useNewUrlParser : true,
+useUnifiedTopology: true,
+//useFindAndModify : false
+
+})
+
+// connection
+const connection = mongoose.connection;
+connection.once("open",()=> {   // arrow functions in JS (readbility)
+console.log("Mongodb Connection Success");
+
+})
+
+const pickupRouter = require("./routes/pickups.js");
+
+
+
+app.use("/pickup",pickupRouter);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
