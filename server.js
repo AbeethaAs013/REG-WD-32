396b16c5c2457a7a0c555de07621d95865ec3047
ashenisandeepa <sandeepaasheni@gmail.com>
2022-04-29 14:08:32 +0530

    commit new

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+ 
+
+
+
+const app = express();
+
+//import routes
+const postRoutes =require('./routes/posts');
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+ 
+//route middleware
+app.use(postRoutes);
+
+
+//declare the sever port
+const PORT = 8000;
+
+
+//DB connection
+const DB_URL = "mongodb+srv://Asheni:993080ashe@cluster0.rmhyl.mongodb.net/REG_WD_32?retryWrites=true&w=majority";
+mongoose.connect(DB_URL)
+.then (()=>{
+    console.log("DB connection succesfull");
+
+})
+.catch((err)=> console.log("DB connection error",err));
+
+
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`);
+
+})
